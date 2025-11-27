270c39c4ea8d280fb1a17ab4cd52d63e71a63654
Ramil Yacat <ryacat56@gmail.com>
2025-11-27 14:35:18 +0800

    Added skeleton previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
