a30669e4b9d4d5eb4e2e229ee942d50ec0530d98
Ramil Yacat <ryacat56@gmail.com>
2025-12-04 13:20:59 +0800

    fixed protected routes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
-const CustomersPage = () => {
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
   return (
-    <div>
-      Customers
-    </div>
-  )
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
-
-export default CustomersPage
